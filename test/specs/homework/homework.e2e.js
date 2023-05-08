64bcccf6ba31d1a8340343b670ddf9f0aee652bc
ilnatvl <ilnatvl@users.noreply.github.com>
2023-05-08 13:57:49 +0200

    homework 3

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,10 +1,29 @@
 describe('Homework', async () => {
 
-    it('should open page and create screenshot', async () => {
+    it('should open page and register new user', async () => {
+        const nameAndSurnameText = 'Karkulka Cervena'
+        const emailText = 'karkulka.cervena@babicka.les.cz'
+        const passwordText = 'nikomunereknu'
+
+        const baseUrl = 'https://team8-2022brno.herokuapp.com/registrace';
 
         await browser.reloadSession();
-        await browser.url('https://team8-2022brno.herokuapp.com/registrace');
-        await browser.saveScreenshot('registration_page.png');
+        await browser.url(baseUrl);
+
+        const nameAndSurnameField = $('#name');
+        await nameAndSurnameField.setValue(nameAndSurnameText);
+
+        const emailField = $('#email');
+        await emailField.setValue(emailText);
+
+        const passwordField = $('#password');
+        await passwordField.setValue(passwordText);
+
+        const passwordConfirmField = $('#password-confirm');
+        await passwordConfirmField.setValue(passwordText);
+
+        const submitButton = $('button.btn.btn-primary');
+        await submitButton.click();
 
     });
 
