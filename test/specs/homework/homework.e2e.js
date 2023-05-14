d8333cc98e615171e648beef0b19039164fa481d
ilnatvl <ilnatvl@users.noreply.github.com>
2023-05-14 15:47:32 +0200

    homework 4

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,9 +1,9 @@
-describe('Homework', async () => {
+describe('Registration page', async () => {
 
-    it('should open page and register new user', async () => {
+    it('should register a new user', async () => {
         const nameAndSurnameText = 'Karkulka Cervena'
-        const emailText = 'karkulka.cervena@babicka.les.cz'
-        const passwordText = 'nikomunereknu'
+        const emailText = 'karkulka.cervena@tester.cz'
+        const passwordText = 'Nikomunereknu123'
 
         const baseUrl = 'https://team8-2022brno.herokuapp.com/registrace';
 
@@ -26,5 +26,73 @@ describe('Homework', async () => {
         await submitButton.click();
 
     });
+
+
+    it('should not allow to register an existing user', async () => {
+        const nameAndSurnameText = 'Karkulka Cervena'
+        const emailText = 'karkulka.cervena@tester.cz'
+        const passwordText = 'Nikomunereknu123'
+
+        const baseUrl = 'https://team8-2022brno.herokuapp.com/registrace';
+
+        await browser.reloadSession();
+        await browser.url(baseUrl);
+
+        const nameAndSurnameField = $('#name');
+        await nameAndSurnameField.setValue(nameAndSurnameText);
+
+        const emailField = $('#email');
+        await emailField.setValue(emailText);
+
+        const passwordField = $('#password');
+        await passwordField.setValue(passwordText);
+
+        const passwordConfirmField = $('#password-confirm');
+        await passwordConfirmField.setValue(passwordText);
+
+        const submitButton = $('button.btn.btn-primary');
+        await submitButton.click();
+
+        const registrationErrors = await $$('span.invalid-feedback');
+        for (const row of registrationErrors) {
+            console.log('Registration error: ' + await row.getText());
+        }
+
+    });
+
+    it('should not allow registration with invalid password', async () => {
+
+        const nameAndSurnameText = 'Karkulka Cervena'
+        const emailText = 'karkulka.cervena@tester.cz'
+        const passwordText = 'Nikomunereknu'
+
+        const baseUrl = 'https://team8-2022brno.herokuapp.com/registrace';
+
+        await browser.reloadSession();
+        await browser.url(baseUrl);
+
+        const nameAndSurnameField = $('#name');
+        await nameAndSurnameField.setValue(nameAndSurnameText);
+
+        const emailField = $('#email');
+        await emailField.setValue(emailText);
+
+        const passwordField = $('#password');
+        await passwordField.setValue(passwordText);
+
+        const passwordConfirmField = $('#password-confirm');
+        await passwordConfirmField.setValue(passwordText);
+
+        const submitButton = $('button.btn.btn-primary');
+        await submitButton.click();
+
+        const registrationErrors = await $$('span.invalid-feedback');
+        for (const row of registrationErrors) {
+            console.log('Registration error: ' + await row.getText());
+        }
+
+
+    });
+
 
 }); 
