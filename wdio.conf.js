dc0dfcca077e97ad56ed5f002f8485ddd46cfa71
ilnatvl <ilnatvl@users.noreply.github.com>
2023-06-11 11:29:03 +0200

    homework 6

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,8 @@
+import allure from 'allure-commandline';
+const allureTmpDirectory = './.tmp/allure';
+const allureReportDirectory = './reports/allure';
 export const config = {
+    automationProtocol: 'devtools',
     runner: 'local',
     specs: [
         './test/specs/*.e2e.js'
@@ -54,9 +58,36 @@ export const config = {
         'geckodriver'
     ],
     framework: 'mocha',
-    reporters: ['spec'],
+    reporters: [
+        'spec',
+        ['allure', {
+            outputDir: allureTmpDirectory,
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: true,
+            addConsoleLogs: true,
+        }]
+    ],
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
+    },
+    onPrepare: (config, capabilities) => {
+        // remove previous tmp files
+        fs.rmdir(allureTmpDirectory, { recursive: true }, err => {
+            if (err) console.log(err);
+        });
+    },
+    onComplete: () => {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', '--clean', allureTmpDirectory, '--output', allureReportDirectory]);
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(() => reject(reportError), 5000);
+            generation.on('exit', function (exitCode) {
+                clearTimeout(generationTimeout);
+                if (exitCode !== 0) return reject(reportError);
+                console.log('Allure report successfully generated');
+                resolve()
+            });
+        });
     }
 }
