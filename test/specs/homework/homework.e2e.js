af68f5e2ab43a58ca40a44f685f643574dce578c
ilnatvl <ilnatvl@users.noreply.github.com>
2023-06-12 20:06:25 +0200

    homework 7

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -5,27 +5,13 @@ describe('Registration page', async () => {
         const emailText = 'karkulka.cervena@tester.cz'
         const passwordText = 'Nikomunereknu123'
 
-        const baseUrl = 'https://team8-2022brno.herokuapp.com/registrace';
+        await openRegistrationPage();
+        await setName(nameAndSurnameText);
+        await setEmail(emailText);
+        await setPassword(passwordText);
+        await submitForm();
 
-        await browser.reloadSession();
-        await browser.url(baseUrl);
-
-        const nameAndSurnameField = $('#name');
-        await nameAndSurnameField.setValue(nameAndSurnameText);
-
-        const emailField = $('#email');
-        await emailField.setValue(emailText);
-
-        const passwordField = $('#password');
-        await passwordField.setValue(passwordText);
-
-        const passwordConfirmField = $('#password-confirm');
-        await passwordConfirmField.setValue(passwordText);
-
-        const submitButton = $('button.btn.btn-primary');
-        await submitButton.click();
-
-        const usernameMenu = $('.navbar-right .dropdown-toggle');
+        const usernameMenu = getUserNameMenu();
         await expect(usernameMenu).toBeDisplayed();
         await expect(usernameMenu).toHaveText(nameAndSurnameText); 
 
@@ -38,28 +24,13 @@ describe('Registration page', async () => {
         const passwordText = 'Nikomunereknu123'
         const errorText = 'Účet s tímto emailem již existuje'
 
+        await openRegistrationPage();
+        await setName(nameAndSurnameText);
+        await setEmail(emailText);
+        await setPassword(passwordText);
+        await submitForm();
 
-        const baseUrl = 'https://team8-2022brno.herokuapp.com/registrace';
-
-        await browser.reloadSession();
-        await browser.url(baseUrl);
-
-        const nameAndSurnameField = $('#name');
-        await nameAndSurnameField.setValue(nameAndSurnameText);
-
-        const emailField = $('#email');
-        await emailField.setValue(emailText);
-
-        const passwordField = $('#password');
-        await passwordField.setValue(passwordText);
-
-        const passwordConfirmField = $('#password-confirm');
-        await passwordConfirmField.setValue(passwordText);
-
-        const submitButton = $('button.btn.btn-primary');
-        await submitButton.click();
-
-        const registrationError = await $('span.invalid-feedback');
+        const registrationError = await getRegistrationError();
         await expect(registrationError).toBeDisplayed();
         await expect(registrationError).toHaveText(errorText);
 
@@ -72,31 +43,55 @@ describe('Registration page', async () => {
         const passwordText = 'Nikomunereknu'
         const errorText = 'Heslo musí obsahovat minimálně 6 znaků, velké i malé písmeno a číslici'
 
-        const baseUrl = 'https://team8-2022brno.herokuapp.com/registrace';
+        await openRegistrationPage();
+        await setName(nameAndSurnameText);
+        await setEmail(emailText);
+        await setPassword(passwordText);
+        await submitForm();
 
-        await browser.reloadSession();
-        await browser.url(baseUrl);
-
-        const nameAndSurnameField = $('#name');
-        await nameAndSurnameField.setValue(nameAndSurnameText);
-
-        const emailField = $('#email');
-        await emailField.setValue(emailText);
-
-        const passwordField = $('#password');
-        await passwordField.setValue(passwordText);
-
-        const passwordConfirmField = $('#password-confirm');
-        await passwordConfirmField.setValue(passwordText);
-
-        const submitButton = $('button.btn.btn-primary');
-        await submitButton.click();
-
-        const registrationError = await $('span.invalid-feedback');
+        const registrationError = await getRegistrationError();
         await expect(registrationError).toBeDisplayed();
         await expect(registrationError).toHaveText(errorText);
 
     });
 
-
 }); 
+
+async function getRegistrationError() {
+    return await $('span.invalid-feedback');
+}
+
+async function setName(nameAndSurnameText) {
+    const nameAndSurnameField = $('#name');
+    await nameAndSurnameField.setValue(nameAndSurnameText);
+}
+
+function getUserNameMenu() {
+    return $('.navbar-right .dropdown-toggle');
+}
+
+async function submitForm() {
+    const submitButton = $('button.btn.btn-primary');
+    await submitButton.click();
+}
+
+async function setPassword(passwordText) {
+    const passwordField = $('#password');
+    await passwordField.setValue(passwordText);
+
+    const passwordConfirmField = $('#password-confirm');
+    await passwordConfirmField.setValue(passwordText);
+}
+
+async function setEmail(emailText) {
+    const emailField = $('#email');
+    await emailField.setValue(emailText);
+}
+
+async function openRegistrationPage() {
+    const baseUrl = 'https://team8-2022brno.herokuapp.com/registrace';
+
+    await browser.reloadSession();
+    await browser.url(baseUrl);
+}
+
